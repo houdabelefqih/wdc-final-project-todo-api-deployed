74fda05a44bd068d4152b2fe56e16e55aacdb104
Houda Belefqih <houdabelefqih@gmail.com>
2019-10-25 11:39:08 +0100

    Added simple pagination

--- django_todos/django_todos/settings/base.py
+++ django_todos/django_todos/settings/base.py
@@ -141,5 +141,5 @@ REST_FRAMEWORK = {
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.CursorPagination',
 
-    'PAGE_SIZE': 3,
+    'PAGE_SIZE': 1,
 }
